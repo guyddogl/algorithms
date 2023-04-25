a1e12e826e29b8e8201f2e956f0924e567471908
Guyddo <guyddogl@gmail.com>
2023-04-25 15:41:03 -0300

    Requirement 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,12 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(27041990, 2)
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("GUYDDO", "2")
+    assert encrypt_message("GGUUYY", 3) == "UGG_YYU"
+    assert encrypt_message("GUYDDO", 4) == "OD_DYUG"
+    assert encrypt_message("GUYDDOGL", 10) == "LGODDYUG"
