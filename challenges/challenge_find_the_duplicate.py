009404b2f0400b88ce0ab11dbc71008abfc28f12
Guyddo <guyddogl@gmail.com>
2023-04-25 17:55:30 -0300

    Requirement 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    nums.sort()
+
+    for index in range(1, len(nums)):
+        if type(nums[index]) == str or int(nums[index]) < 0:
+            return False
+
+        if nums[index] == nums[index - 1]:
+            return nums[index]
+
+    return False
